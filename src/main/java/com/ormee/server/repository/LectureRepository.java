92a3073c9fbddbdf2995897269ae9c1942af383b
민경 <tjalsrud0101@gmail.com>
2024-11-19 22:11:50 +0900

    feat: code generator, Lecture API, add code column to Teacher (#5)

--- src/main/java/com/ormee/server/repository/LectureRepository.java
+++ src/main/java/com/ormee/server/repository/LectureRepository.java
@@ -1,7 +1,14 @@
 package com.ormee.server.repository;
 
+import com.ormee.server.model.Lecture;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
-public interface LectureRepository {
+public interface LectureRepository extends JpaRepository<Lecture, UUID> {
+    Optional<Lecture> findByCode(Integer code);
+    boolean existsByCode(Integer code);
 }
