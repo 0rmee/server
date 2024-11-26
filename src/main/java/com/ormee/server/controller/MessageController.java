907f7224b7e77863ff220ace10873e44ac5748a8
kimjihyun <kimjihyun@MacBook-Pro.local>
2024-11-26 11:20:30 +0900

    feat: add message API (#9)

--- src/main/java/com/ormee/server/controller/MessageController.java
+++ src/main/java/com/ormee/server/controller/MessageController.java
@@ -1,7 +1,38 @@
 package com.ormee.server.controller;
 
+import com.ormee.server.dto.MessageSubmitDto;
+import com.ormee.server.dto.response.ResponseDto;
+import com.ormee.server.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.UUID;
+
+@RestController
 public class MessageController {
+    private final MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/student/{lectureId}/{memoId}/messages")
+    public ResponseDto submitMessage(
+            @PathVariable UUID lectureId,
+            @PathVariable long memoId,
+            @RequestBody MessageSubmitDto messageSubmitDto
+    ){
+        messageService.submitMessage(lectureId, memoId, messageSubmitDto);
+        return ResponseDto.success();
+    }
+
+    @GetMapping("/teacher/{lectureId}/{memoId}/messages")
+    public ResponseDto getMessages(
+            @PathVariable UUID lectureId,
+            @PathVariable Long memoId
+    ){
+        return ResponseDto.success(messageService.getMessages(lectureId, memoId));
+    }
 }
--- src/main/java/com/ormee/server/service/MemoService.java
+++ src/main/java/com/ormee/server/service/MemoService.java
@@ -35,7 +35,6 @@ public class MemoService {
     }
 
     private Integer getSubmitCount(Long memoId) {
-
         return messageRepository.countByMemoId(memoId);
     }
 
