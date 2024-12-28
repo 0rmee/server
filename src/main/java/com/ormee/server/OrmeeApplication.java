046c4c4eafb284a4058a8b4d80a435fb67e0f377
민경 <tjalsrud0101@gmail.com>
2024-12-28 18:54:43 +0900

    chore: set TimeZone

--- src/main/java/com/ormee/server/OrmeeApplication.java
+++ src/main/java/com/ormee/server/OrmeeApplication.java
@@ -1,12 +1,20 @@
 package com.ormee.server;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class OrmeeApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrmeeApplication.class, args);
