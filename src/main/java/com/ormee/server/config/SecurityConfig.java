497276ab44a1297553527d0b692133a2d2c17412
민경 <tjalsrud0101@gmail.com>
2024-12-30 17:29:22 +0900

    fix: cors error

--- src/main/java/com/ormee/server/config/SecurityConfig.java
+++ src/main/java/com/ormee/server/config/SecurityConfig.java
@@ -30,7 +30,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000", "https://ormee-mvp.web.app", "https://ormee-mvp.firebaseapp.com"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://127.0.0.1:3000",
+                "https://ormee-mvp.web.app",
+                "https://ormee-mvp.firebaseapp.com",
+                "http://52.78.13.49:8080"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
