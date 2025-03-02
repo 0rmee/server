cd844cbd4ceeaea20b89adf05b4d6eea06e1e8b6
kimjihyun <kimjihyun@MacBook-Pro.local>
2025-03-02 11:29:15 +0900

    feat: notification 틀 작성, 추후 구현 (#8)

---
+++ src/main/java/com/ormee/server/service/NotificationService.java
@@ -0,0 +1,16 @@
+package com.ormee.server.service;
+
+import com.ormee.server.repository.LectureRepository;
+import com.ormee.server.repository.NotificationRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+    private final NotificationRepository notificationRepository;
+    private final LectureRepository lectureRepository;
+
+    public NotificationService(NotificationRepository notificationRepository, LectureRepository lectureRepository) {
+        this.notificationRepository = notificationRepository;
+        this.lectureRepository = lectureRepository;
+    }
+}
