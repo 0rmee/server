c8e1d939064260907d1efe1c81da8058ab4df15e
kimjihyun <kimjihyun@MacBook-Pro.local>
2025-03-27 16:52:34 +0900

    feat: RefreshToken entity 추가

---
+++ src/main/java/com/ormee/server/config/jwt/RefreshToken.java
@@ -0,0 +1,20 @@
+package com.ormee.server.config.jwt;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class RefreshToken {
+    @Id
+    private String username;
+
+    private String refreshToken;
+}
