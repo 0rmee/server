be482b6ee1c535839984526076c469f3ef9721c4
민경 <tjalsrud0101@gmail.com>
2025-05-07 01:49:17 +0900

    fix: lecture member 매핑 관계 정리, 서비스 로직 수정 (#26)

--- src/main/java/com/ormee/server/controller/LectureController.java
+++ src/main/java/com/ormee/server/controller/LectureController.java
@@ -4,67 +4,52 @@ import com.ormee.server.dto.lecture.LectureRequestDto;
 import com.ormee.server.dto.response.ResponseDto;
 import com.ormee.server.service.LectureService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/teachers")
 public class LectureController {
-    // 추후 code->lectureId로 전부 수정
 
     @Autowired
     private LectureService lectureService;
 
-    @PostMapping("/{teacherId}/lectures")
-    public ResponseDto createLecture(@RequestBody LectureRequestDto lectureRequestDto, @PathVariable Integer teacherId) {
-        return ResponseDto.success(lectureService.saveLecture(lectureRequestDto, teacherId));
+    @PostMapping("/lectures")
+    public ResponseDto createLecture(@RequestBody LectureRequestDto lectureRequestDto, Authentication authentication) {
+        lectureService.save(lectureRequestDto, authentication.getName());
+        return ResponseDto.success();
     }
 
     @PutMapping("/lectures/{lectureId}") // 강의 수정 추후 추가
-    public ResponseDto updateLecture(@RequestBody LectureRequestDto lectureRequestDto, @PathVariable Integer lectureId) {
+    public ResponseDto updateLecture(@RequestBody LectureRequestDto lectureRequestDto, @PathVariable Long lectureId, Authentication authentication) {
+        lectureService.update(lectureRequestDto, lectureId, authentication.getName());
         return ResponseDto.success();
     }
 
     @DeleteMapping("/lectures/{lectureId}")
-    public ResponseDto deleteLecture(@PathVariable Integer lectureId) {
-        lectureService.delete(lectureId);
+    public ResponseDto deleteLecture(@PathVariable Long lectureId, Authentication authentication) {
+        lectureService.delete(lectureId, authentication.getName());
         return ResponseDto.success();
     }
 
-    @GetMapping("/{teacherid}/lectures")
-    public ResponseDto getLectureList(@PathVariable Integer teacherId) {
-        return ResponseDto.success(lectureService.findAllLectures(teacherId));
+    @GetMapping("/lectures")
+    public ResponseDto getLectureList(Authentication authentication) {
+        return ResponseDto.success(lectureService.findAllLectures(authentication.getName()));
     }
 
-    @GetMapping("/lectures/{lectureId}/students")
-    public ResponseDto getStudentList(@PathVariable Integer lectureId) {
-        return ResponseDto.success(); // 추후 서비스로직 추가
-    }
-
-    @PutMapping("/lectures/{lectureId}/students/{studentId}")
-    public ResponseDto updateStudentMemo(@PathVariable Integer lectureId, @PathVariable Integer studentId) {
-        return ResponseDto.success(); // 추후 서비스로직 추가
-    }
-
-    @DeleteMapping("/lectures/{lectureId}/students/{studentId}")
-    public ResponseDto deleteStudentMemo(@PathVariable Integer lectureId, @PathVariable Integer studentId) {
-        return ResponseDto.success(); // 추후 서비스로직 추가
-    }
-
-    // 아래 코드 처분은 어떻게 할까요??
-
-    @GetMapping("/{code}") // 추후 강의홈 uri 방식 정해지면 그때 수정
-    public ResponseDto getLecture(@PathVariable Integer code) {
-        return ResponseDto.success(lectureService.findLectureByCode(code));
-    }
-
-    @PutMapping("/{code}")
-    public ResponseDto closeLecture(@PathVariable Integer code) {
-        lectureService.close(code);
-        return ResponseDto.success();
-    }
-
-    @GetMapping("/{code}/validate")
-    public ResponseDto validateLecture(@PathVariable Integer code) {
-        return ResponseDto.success(lectureService.validate(code));
-    }
+//    @GetMapping("/{code}") // 추후 강의홈 uri 방식 정해지면 그때 수정
+//    public ResponseDto getLecture(@PathVariable Integer code) {
+//        return ResponseDto.success(lectureService.findLectureByCode(code));
+//    }
+//
+//    @PutMapping("/{code}")
+//    public ResponseDto closeLecture(@PathVariable Integer code) {
+//        lectureService.close(code);
+//        return ResponseDto.success();
+//    }
+//
+//    @GetMapping("/{code}/validate")
+//    public ResponseDto validateLecture(@PathVariable Integer code) {
+//        return ResponseDto.success(lectureService.validate(code));
+//    }
 }
