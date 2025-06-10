97596d29e70dca2c99f64017e657885ec1371810
민경 <tjalsrud0101@gmail.com>
2025-06-10 13:52:36 +0900

    feat: 알림 목록, 모두 읽음, 모두 삭제, 개별 읽음, 개별 삭제 (#38)

--- src/main/java/com/ormee/server/repository/NotificationRepository.java
+++ src/main/java/com/ormee/server/repository/NotificationRepository.java
@@ -1,9 +1,18 @@
 package com.ormee.server.repository;
 
+import com.ormee.server.model.Lecture;
 import com.ormee.server.model.Notification;
+import com.ormee.server.model.NotificationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findAllByLectureOrderByCreatedAtDesc(Lecture lecture);
+    List<Notification> findAllByLectureAndTypeOrderByCreatedAtDesc(Lecture lecture, NotificationType type);
+
+    void deleteAllByLecture(Lecture lecture);
+
 }
--- src/main/java/com/ormee/server/service/NotificationService.java
+++ src/main/java/com/ormee/server/service/NotificationService.java
@@ -1,8 +1,17 @@
 package com.ormee.server.service;
 
+import com.ormee.server.dto.notification.NotificationDto;
+import com.ormee.server.exception.CustomException;
+import com.ormee.server.exception.ExceptionType;
+import com.ormee.server.model.Lecture;
+import com.ormee.server.model.Notification;
+import com.ormee.server.model.NotificationType;
 import com.ormee.server.repository.LectureRepository;
 import com.ormee.server.repository.NotificationRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NotificationService {
@@ -14,5 +23,55 @@ public class NotificationService {
         this.lectureRepository = lectureRepository;
     }
 
-    // 알림 구현
+    public List<NotificationDto> getNotificationsByLectureIdAndType(Long lectureId, String filter) {
+        Lecture lecture = lectureRepository.findById(lectureId).orElseThrow(() -> new CustomException(ExceptionType.LECTURE_NOT_FOUND_EXCEPTION));
+        List<Notification> notifications = switch (filter) {
+            case "퀴즈" ->
+                    notificationRepository.findAllByLectureAndTypeOrderByCreatedAtDesc(lecture, NotificationType.Quiz);
+            case "쪽지" ->
+                    notificationRepository.findAllByLectureAndTypeOrderByCreatedAtDesc(lecture, NotificationType.Memo);
+            case "숙제" ->
+                    notificationRepository.findAllByLectureAndTypeOrderByCreatedAtDesc(lecture, NotificationType.Assignment);
+            case "질문" ->
+                    notificationRepository.findAllByLectureAndTypeOrderByCreatedAtDesc(lecture, NotificationType.QnA);
+            default -> notificationRepository.findAllByLectureOrderByCreatedAtDesc(lecture);
+        };
+
+        return notifications.stream()
+                .map(notification -> NotificationDto.builder()
+                        .notificationId(notification.getId())
+                        .type(String.valueOf(notification.getType()))
+                        .title(notification.getTitle())
+                        .description(notification.getDescription())
+                        .parentId(notification.getParentId())
+                        .isRead(notification.getIsRead() != null && notification.getIsRead())
+                        .createdAt(notification.getCreatedAt())
+                        .build())
+                .toList();
+    }
+
+    @Transactional
+    public void markAllAsRead(Long lectureId) {
+        Lecture lecture = lectureRepository.findById(lectureId).orElseThrow(() -> new CustomException(ExceptionType.LECTURE_NOT_FOUND_EXCEPTION));
+        List<Notification> notifications = notificationRepository.findAllByLectureOrderByCreatedAtDesc(lecture);
+
+        notifications.forEach(notification -> notification.setIsRead(true));
+    }
+
+    public void deleteAll(Long lectureId) {
+        Lecture lecture = lectureRepository.findById(lectureId).orElseThrow(() -> new CustomException(ExceptionType.LECTURE_NOT_FOUND_EXCEPTION));
+        notificationRepository.deleteAllByLecture(lecture);
+    }
+
+    @Transactional
+    public void markAsRead(Long notificationId) {
+        Notification notification = notificationRepository.findById(notificationId).orElseThrow(() -> new CustomException(ExceptionType.NOTIFICATION_NOT_FOUND_EXCEPTION));
+        notification.setIsRead(true);
+    }
+
+
+    public void deleteById(Long notificationId) {
+        Notification notification = notificationRepository.findById(notificationId).orElseThrow(() -> new CustomException(ExceptionType.NOTIFICATION_NOT_FOUND_EXCEPTION));
+        notificationRepository.delete(notification);
+    }
 }
