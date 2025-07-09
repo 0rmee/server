c5d49538bb35488ede3ecfa20a1af552e74e1264
민경 <tjalsrud0101@gmail.com>
2025-07-09 17:42:13 +0900

    feat: isOwner 필드 추가

--- src/main/java/com/ormee/server/home/controller/TeacherHomeController.java
+++ src/main/java/com/ormee/server/home/controller/TeacherHomeController.java
@@ -2,6 +2,7 @@ package com.ormee.server.home.controller;
 
 import com.ormee.server.global.response.ResponseDto;
 import com.ormee.server.home.service.HomeService;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class TeacherHomeController {
     }
 
     @GetMapping("/home/{lectureId}")
-    public ResponseDto getHomeInfo(@PathVariable Long lectureId) {
-        return ResponseDto.success(homeService.getInfo(lectureId));
+    public ResponseDto getHomeInfo(@PathVariable Long lectureId, Authentication authentication) {
+        return ResponseDto.success(homeService.getInfo(lectureId, authentication.getName()));
     }
 }
