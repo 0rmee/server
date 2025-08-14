1cdeed786bc7acbbf9eca1c4b6e463e23c9d13ce
민경 <tjalsrud0101@gmail.com>
2025-08-14 16:30:28 +0900

    fix: 생성 기능 트랜잭션 처리

--- src/main/java/com/ormee/server/homework/service/HomeworkService.java
+++ src/main/java/com/ormee/server/homework/service/HomeworkService.java
@@ -58,6 +58,7 @@ public class HomeworkService {
         this.studentNotificationService = studentNotificationService;
     }
 
+    @Transactional
     public void create(Long lectureId, HomeworkSaveDto homeworkSaveDto, String username) throws Exception {
         Lecture lecture = lectureRepository.findById(lectureId)
                 .orElseThrow(() -> new CustomException(ExceptionType.LECTURE_NOT_FOUND_EXCEPTION));
@@ -187,6 +188,7 @@ public class HomeworkService {
                 .build();
     }
 
+    @Transactional
     public void update(Long homeworkId, HomeworkSaveDto homeworkSaveDto) throws Exception {
         Homework homework = homeworkRepository.findById(homeworkId)
                 .orElseThrow(() -> new CustomException(ExceptionType.HOMEWORK_NOT_FOUND_EXCEPTION));
